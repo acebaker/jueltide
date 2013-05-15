cbaa6e43f0f63fcba6a94790244a9103fc56f95f
Jeremy Juel <jeremyjuel@Jeremys-iMac.local>
2013-05-14 22:31:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jueltide::Application.config.secret_token = '1d62fa5e9b7b3e4387a10b2ad614218ab151cf1f266fe961a1e9819a68de19917a05113f7001e135d092791ee4803bf5311dfa9ca3323c90e5832d4a9e341983'
