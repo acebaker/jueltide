ea75c7584bc40ba0430e436c96a7c5fb459a5af2
Jeremy Juel <jeremyjuel@Jeremys-iMac.local>
2013-05-14 23:28:37 -0700

    Finish static pages in tutorial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,29 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = 'Home'
   end
 
   def services
+    @title = 'Services'
   end
 
   def tideGates101
+    @title = 'Tide Gates 101'
   end
 
   def caseStudies
+    @title = 'Case Studies'
   end
 
   def tideBlog
+    @title = 'Tide Blog'
   end
 
   def aboutUs
+    @title = 'About Us'
   end
 
   def contactUs
+    @title = 'Contact Us'
   end
 end
